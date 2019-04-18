e2d5ee398b9bf5bb1c28247154feae0ca1c4c52b
djeisu <djheyson.oliver@gmail.com>
2019-04-17 22:03:52 -0300

    config cors for method put

--- config/application.rb
+++ config/application.rb
@@ -32,12 +32,11 @@ module XSunit
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-
     # CORS
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put]
       end
     end
 
